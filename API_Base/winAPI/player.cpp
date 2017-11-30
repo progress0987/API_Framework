eea49da19319417761fe806090415126d7a585a0
leejiyeon <any310@naver.com>
2017-11-30 14:15:09 +0900

    ㅊㄹㅇㅌㄴ

--- API_Base/winAPI/player.cpp
+++ API_Base/winAPI/player.cpp
@@ -539,6 +539,7 @@ for (int i = 0; i < monincurmap.size(); i++) {
 	///////////////////////////////////////////////////Å×½ºÆ®
 	if (KEYMANAGER->isOnceKeyDown(VK_SPACE)) {
 		GainExp(10000);
+		SOUNDMANAGER->play("level");
 	}
 
 
